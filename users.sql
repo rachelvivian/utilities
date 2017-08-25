d205dcc33440dd6871b3ced30a37135e547ed024
Rachel <rachviv@gmail.com>
2017-08-25 01:39:48 +0000

    Reset script added.

--- users.sql
+++ users.sql
@@ -1,56 +1,51 @@
--- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: c9
--- ------------------------------------------------------
--- Server version	5.5.53-0ubuntu0.14.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) DEFAULT NULL,
-  `password` text,
-  `secret` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+	`id` int NOT NULL AUTO_INCREMENT,
+	`username` varchar(32) NOT NULL UNIQUE,
+	`password` TEXT NOT NULL,
+	`email` varchar(64) NOT NULL UNIQUE,
+	`favorite_pet` varchar(32),
+	PRIMARY KEY (`id`)
+);
 
---
--- Dumping data for table `users`
---
+CREATE TABLE `pets` (
+	`name` varchar(32) NOT NULL,
+	`type` int NOT NULL,
+	`age` int(3) NOT NULL,
+	`gender` bool,
+	`owner` int NOT NULL,
+	PRIMARY KEY (`name`,`owner`)
+);
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES 
-(1,'devon','password','secret_hash'),
-(2,'zack','password','something'),
-(3,'zack','password','other_data');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `pet_types` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`family` varchar(16) NOT NULL,
+	`breed` varchar(16),
+	PRIMARY KEY (`id`)
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE `cars` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`make` varchar(16) NOT NULL,
+	`model` varchar(16) NOT NULL,
+	`year` int(4) NOT NULL,
+	`color` varchar(16) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
--- Dump completed on 2017-08-22  2:06:34
+CREATE TABLE `user_cars` (
+	`user_id` int NOT NULL,
+	`car_id` int NOT NULL,
+	PRIMARY KEY (`user_id`,`car_id`)
+);
+
+ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`favorite_pet`) REFERENCES `pets`(`name`);
+
+ALTER TABLE `pets` ADD CONSTRAINT `pets_fk0` FOREIGN KEY (`type`) REFERENCES `pet_types`(`id`);
+
+ALTER TABLE `pets` ADD CONSTRAINT `pets_fk1` FOREIGN KEY (`owner`) REFERENCES `users`(`id`);
+
+ALTER TABLE `user_cars` ADD CONSTRAINT `user_cars_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `user_cars` ADD CONSTRAINT `user_cars_fk1` FOREIGN KEY (`car_id`) REFERENCES `cars`(`id`);
