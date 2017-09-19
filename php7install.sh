25d2420dfd748d727c3fc312fa88f052b4464385
Rachel <rachviv@gmail.com>
2017-09-19 00:58:05 +0000

    PHP7 Install script

---
+++ php7install.sh
@@ -0,0 +1,6 @@
+add-apt-repository ppa:ondrej/php -y
+apt-get update -y
+
+apt-get install -y php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-mcrypt php7.0-json php7.0-mysql php7.0-opcache php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-xml
+
+apt-get install -y libapache2-mod-php7.0
